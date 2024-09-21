40f7ac981a58ee0dfb798fd5164201d3a2c90bc1
Sadiya Khan <sshoaibkhan861@gmail.com>
2024-09-21 16:06:22 +0530

    changes in routes

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 const { error } = require('console');
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
-const dbUrl = process.env.ATLASDB_URL;
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust"
+// const dbUrl = process.env.ATLASDB_URL;
 
 main()
 .then((res)=>{
@@ -35,7 +35,7 @@ main()
 });
 
 async function main() {
-   await mongoose.connect(dbUrl);
+   await mongoose.connect(MONGO_URL);
 };
 
 app.set("view engine" , "ejs");
@@ -46,7 +46,7 @@ app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname,"/public")));
 
 const store = MongoStore.create({
-    mongoUrl: dbUrl,
+    mongoUrl: MONGO_URL, //
     crypto: {
         secret: process.env.SECRET,
       },
@@ -69,9 +69,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi,I am root");
-// });
+app.get("https://wanderlust-42ul.onrender.com",(req,res)=>{
+    res.redirect("/listings");
+});
 
 
 
